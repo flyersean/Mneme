a048258341b6009dae4065a611450298a15eb1f0
flyersean <3701074@gmail.com>
2026-08-08 22:35:13 -0400

    setup: reopen stdin from /dev/tty to fix curl|bash pipe EOF

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 # Mneme one-line setup: curl -sSL <url> | bash
 set -e
 
+# Reopen stdin from terminal (curl pipe closes)
+exec < /dev/tty
+
 echo "=== Mneme Setup ==="
 
 # Clone Mneme if not present
